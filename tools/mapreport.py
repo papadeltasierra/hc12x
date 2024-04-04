1655cc5cfac2fa59e9a8d8b50c8ab0deeb5a36d1
Paul D.Smith <paul@pauldsmith.org.uk>
2024-04-04 19:02:02 +0100

    Split near and tiny RAM

--- tools/mapreport.py
+++ tools/mapreport.py
@@ -1,7 +1,7 @@
 """Small script to add up resource usage from map file."""
 import sys
 import re
-from typing import List
+from typing import List, Tuple
 import argparse
 
 SEGMENTS = r"segments"
@@ -17,12 +17,67 @@ RGX_SEGDATA = re.compile(
     LENGTH +
     r">\d+)\s+segment\s+(?P<" + NAME + r">.+)",
     re.IGNORECASE)
+RGX_HEX_RANGE = re.compile(r"^(?P<start>0x[0-9a-f]+),(?P<end>0x[0-9a-f])$", re.IGNORECASE)
 
 SEGMENTS_TO_IGNORE=[".debug", ".info."]
+
+class Resource:
+    def __init__(self, desc, start_addr, end_addr):
+        self._desc = desc
+        self._start = start_addr
+        self._end = end_addr
+        self._min = end_addr
+        self._max = start_addr
+        self._length = 0
+
+    def add(self, start_addr, end_addr):
+        self._length += end_addr - start_addr - 1
+        self._min = min(self._min, start_addr)
+        self._max = max(self._max, end_addr)
+
+    @property
+    def desc(self):
+        return self._desc
+
+    @property
+    def length(self):
+        return self._length
+
+    @property
+    def start(self):
+        return self._start
+
+    @property
+    def end(self):
+        return self._end
+
+    @property
+    def max(self):
+        if self.length > 0:
+            return self._max
+        else:
+            return self._end
+
+    @property
+    def min(self):
+        if self.length > 0:
+            return self._min
+        else:
+            return self._start
+
+    @property
+    def waste(self):
+        if self.length > 0:
+            return self.max - self.min - self.length
+        else:
+            return 0
+
+resources: List[Resource] = []
 
 
 def analyze(args: argparse.Namespace) -> None:
     """Analyze the mapfile."""
+    global resources
     with open(args.mapfile, "r") as mapfile:
         mapdata = mapfile.read()
 
@@ -42,15 +97,6 @@ def analyze(args: argparse.Namespace) -> None:
         }
 
     # Add up the resources, ignoring resources that are not present.
-    rom = 0
-    romMax = 0
-    romMin = 0xFFFFFFFF
-    ram = 0
-    ramMax = 0
-    ramMin = 0xFFFFFFFF
-    eeprom = 0
-    eepromMax = 0
-    eepromMin = 0xFFFFFFFF
 
     for key, segValue in segmap.items():
         if key in SEGMENTS_TO_IGNORE:
@@ -59,51 +105,45 @@ def analyze(args: argparse.Namespace) -> None:
         if segValue[LENGTH] == 0:
             continue
 
-        if segValue[START] >= args.rom:
-            rom += segValue[LENGTH]
-            romMin = min(romMin, segValue[START])
-            romMax = max(romMax, segValue[END])
-        elif segValue[START] >= args.eeprom:
-            eeprom += segValue[LENGTH]
-            eepromMin = min(eepromMin, segValue[START])
-            eepromMax = max(eepromMax, segValue[END])
-        else:
-            ram += segValue[LENGTH]
-            ramMin = min(ramMin, segValue[START])
-            ramMax = max(ramMax, segValue[END])
+        for resource in resources:
+            if segValue[START] >= resource.start:
+                resource.add(segValue[START], segValue[END])
+                break
 
-    if ram == 0:
-        ramMin = args.ram
-        ramMax = args.ram
-    if eeprom == 0:
-        eepromMin = args.eeprom
-        eepromMax = args.eeprom
-    if rom == 0:
-        romMin = args.rom
-        romMax = args.rom
-
-    print("Resource: start     end      length  waste")
-    print("RAM:      %8.8X, %8.8X, %5d, %5d" % (ramMin, ramMax, ram, (ramMax - ramMin - ram)))
-    print("EEPROM:   %8.8X, %8.8X, %5d, %5d" % (eepromMin, eepromMax, eeprom, (eepromMax - eepromMin - eeprom)))
-    print("ROM:      %8.8X, %8.8X, %5d, %5d" % (romMin, romMax, rom, (romMax - romMin - rom)))
+    resources.reverse()
+    print("Resource:   start     end      length  waste remains")
+    for resource in resources:
+        print("%-10s: %8.8X, %8.8X, %5d, %5d %5d" % (resource.desc, resource.min, resource.max, resource.length, (resource.max - resource.min - resource.length), (resource.end - resource.start - resource.length)))
 
 def parse_args(argv: List[str]) -> argparse.Namespace:
     """Parse command  line arguments"""
-    def hexint(arg: str) -> int:
+    def hexrange(arg: str) -> Tuple[int, int]:
         """Validate and parse a headecimal string"""
         try:
-            return hex(arg)
+            match = RGX_HEX_RANGE.match(arg)
+            assert match
+            return hex(match.group("start")), hex(match.group("end"))
         except:
-            argparse.ArgumentTypeError("Expected a hexadecimal value")
+            argparse.ArgumentTypeError("Expected a hexadecimal range e.g. '0x1234,0x5678'")
 
     parser = argparse.ArgumentParser("Create COSMIC linker mapfile report")
-    parser.add_argument("-a", "--ram", help="Starting address of RAM", type=hexint, default=0)
-    parser.add_argument("-e", "--eeprom", help="Starting address of EEPROM", type=hexint, default=0x4000)
-    parser.add_argument("-o", "--rom", help="Starting address of ROM", type=hexint, default=0x8000)
+    parser.add_argument("-t", "--tiny-ram", help="Starting address of RAM", type=hexrange, default=(0,0x0100))
+    parser.add_argument("-n", "--near-ram", help="Starting address of RAM", type=hexrange, default=(0x0100,0x0400))
+    parser.add_argument("-e", "--eeprom", help="Starting address of EEPROM", type=hexrange, default=(0x4000,0x407f))
+    parser.add_argument("-o", "--rom", help="Starting address of ROM", type=hexrange, default=(0x8000,0x9FFF))
     parser.add_argument("mapfile", help="COSMIC linker mapfile.")
     args: argparse.Namespace = parser.parse_args(argv)
-    if ((args.rom < args.eeprom) or (args.eeprom < args.ram)):
+    if ((args.rom < args.eeprom) or (args.eeprom < args.near_ram) or (args.near_ram < args.tiny_ram)):
         argparse.ArgumentError("Expected RAM, EEPROM, ROM spaces in that order")
+
+    tiny_ram = Resource("RAM (tiny)", args.tiny_ram[0], args.tiny_ram[1])
+    near_ram = Resource("RAM (near)", args.near_ram[0], args.near_ram[1])
+    eeprom = Resource("EEPROM", args.eeprom[0], args.eeprom[1])
+    rom = Resource("ROM", args.rom[0], args.rom[1])
+    resources.append(rom)
+    resources.append(eeprom)
+    resources.append(near_ram)
+    resources.append(tiny_ram)
 
     return args
 
